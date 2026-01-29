cfabeb52c07e53c6cbbd0ac89adea97c969da51e
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2026-01-29 11:16:39 +0600

    grpc header & trailers setter added

---
+++ response.go
@@ -0,0 +1,34 @@
+package response
+
+import (
+	"context"
+	"strconv"
+	"sync"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var httpHeaderKey = "x-http-status"
+var httpHeaderKeySetOnce sync.Once
+
+func SetHttpHeaderKey(key string) {
+	httpHeaderKeySetOnce.Do(func() {
+		httpHeaderKey = key
+	})
+}
+
+func SetHeaders(ctx context.Context, headers metadata.MD) error {
+	return grpc.SetHeader(ctx, headers)
+}
+
+func SetTrailers(ctx context.Context, trailers metadata.MD) error {
+	return grpc.SetTrailer(ctx, trailers)
+}
+
+func SetHttpStatusHeader(ctx context.Context, code int) error {
+	return SetHeaders(
+		ctx,
+		metadata.Pairs(httpHeaderKey, strconv.Itoa(code)),
+	)
+}
